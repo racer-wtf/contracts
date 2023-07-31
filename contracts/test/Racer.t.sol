2187a685645f3e4c473240e906fa046884c0056c
ChronosX88 <chronosx88@gmail.com>
2023-07-31 17:01:23 +0300

    Implement createCycle test

--- contracts/test/Racer.t.sol
+++ contracts/test/Racer.t.sol
@@ -20,7 +20,34 @@ import "../src/Racer.sol";
 contract Racer2Test is Test {
     Racer market;
 
+    event CycleCreated(
+        address indexed creator,
+        uint256 indexed id,
+        uint256 startingBlock,
+        uint256 blockLength,
+        uint256 votePrice
+    );
+
     function setUp() public {
         market = new Racer();
+    }
+
+    function testCreateCycle(
+        uint256 startingBlock,
+        uint256 blockLength,
+        uint256 votePrice
+    ) public {
+        vm.assume(votePrice > 0);
+
+        // assume no arithmetic overflow
+        unchecked {
+            vm.assume(startingBlock + blockLength >= startingBlock);
+        }
+
+        vm.startPrank(address(1));
+        vm.expectEmit();
+        emit CycleCreated(address(1), 0, startingBlock, blockLength, votePrice);
+        market.createCycle(startingBlock, blockLength, votePrice);
+        vm.stopPrank();
     }
 }
