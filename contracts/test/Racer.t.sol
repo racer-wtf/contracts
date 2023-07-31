d62923829383592ea06a0f3ee000dad9c7d5b4d7
ChronosX88 <chronosx88@gmail.com>
2023-07-31 17:21:51 +0300

    Implement getCycle unit test

--- contracts/test/Racer.t.sol
+++ contracts/test/Racer.t.sol
@@ -50,4 +50,31 @@ contract Racer2Test is Test {
         market.createCycle(startingBlock, blockLength, votePrice);
         vm.stopPrank();
     }
+
+    function testGetCycle(uint256 startingBlock, uint256 blockLength, uint256 votePrice) public {
+        vm.assume(votePrice > 0);
+
+        // assume no arithmetic overflow
+        unchecked {
+            vm.assume(startingBlock + blockLength >= startingBlock);
+        }
+
+        vm.startPrank(address(1));
+        uint256 cycleId = market.createCycle(startingBlock, blockLength, votePrice);
+        (
+            uint256 startingBlock_,
+            uint256 endingBlock_,
+            uint256 votePrice_,
+            address creator,
+            uint256 balance,
+            uint256 totalVotes
+        ) = market.getCycle(cycleId);
+        assertEq(startingBlock, startingBlock_);
+        assertEq(startingBlock+blockLength, endingBlock_);
+        assertEq(votePrice_, votePrice);
+        assertEq(creator, address(1));
+        assertEq(balance, 0);
+        assertEq(totalVotes, 0);
+        vm.stopPrank();
+    }
 }
